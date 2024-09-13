9893e20c617a0b3015a49eecec8c1dcdd9820644
Ben Przybocki <benjamin.przybocki@gmail.com>
2024-09-13 16:38:08 +0200

    generalize to dimension n, add some wlog assumptions

--- kissing_problem.py
+++ kissing_problem.py
@@ -1,43 +1,52 @@
 # from cvc5.pythonic import *
 from z3 import *
-
+import sys
 from pprint import pprint
 
-def abs(x):
-  return If(x<0, -x, x)
-
-k = 10
+def print_model(model,n,k,x):
+    for i in range(k):
+        for j in range(n):
+            print("x[" + str(i) + "][" + str(j) + "]: " + str(model[x[i][j]]))
+assert(len(sys.argv)) == 3
+n = int(sys.argv[1])
+k = int(sys.argv[2])
 
 x = {}
-y = {}
 
 for i in range(k):
-    x[i] = Real("x" + str(i))
-    y[i] = Real("y" + str(i))
+    if i not in x:
+        x[i] = {}
+    for j in range(n):
+        # x[i][j] is the jth coordinate of the ith sphere
+        x[i][j] = Real("x" + str(i) + "," + str(j))
 
 s = Solver()
 
-#s.add(x[0] ** 2 == 2)
-#if s.check() == sat:
-#  print(s.model())
-#exit()
-
-
 # s.set("nl-cov", "true")
 
+s.add(x[0][0] == 1)
+for j in range(1,n):
+    s.add(x[0][j] == 0)
+
+s.add(x[1][0] == 1/2)
+s.add(x[1][1] == Sqrt(3)/2)
+for j in range(2,n):
+    s.add(x[1][j] == 0)
+
 for i in range(k):
-  # s.add(x[i]**2 + y[i]**2 == 2)
-  s.add(x[i]**2 + y[i]**2 == 1)
-
+    dist = 0
+    for j in range(n):
+        dist += x[i][j]**2
+    s.add(dist == 1)
 
 for i in range(k):
-  for j in range(i+1, k):
-    # s.add((x[i] - x[j])**2 + (y[i]-y[j])**2 >= 2)
-    s.add(x[i]*x[j] + y[i]*y[j] <= 1/2)
-
+    for j in range(i+1, k):
+        dot_product = 0
+        for l in range(n):
+            dot_product += x[i][l]*x[j][l]
+        s.add(dot_product <= 1/2)
 
 if s.check() == sat:
-  for k in s.model():
-    print(str(k) + ": " + str(s.model()[k]))
+    print_model(s.model(),n,k,x)
 else:
-  print("unsat")
+    print(s.check())
